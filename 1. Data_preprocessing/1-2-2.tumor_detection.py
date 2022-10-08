4b6a7a811752b8ee0a399f8d9ecfe310c7d91d7c
Cassie07 <31313145+Cassie07@users.noreply.github.com>
2022-10-08 16:24:23 -0500

    Add files via upload

--- 1. Data_preprocessing/1-2-2.tumor_detection.py
+++ 1. Data_preprocessing/1-2-2.tumor_detection.py
@@ -236,16 +236,20 @@ def train_model(model, criterion, optimizer, num_epochs, count, memo, fold_num, 
 
 
 transform_train = transforms.Compose([
+            transforms.RandomResizedCrop(224)
             transforms.RandomHorizontalFlip(),
             transforms.RandomVerticalFlip(),
             transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
 
 transform_test = transforms.Compose([
+            transforms.Resize(224),
             transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
+
+
 batch_size= 64
 num_epochs= 40
 num_class=2
